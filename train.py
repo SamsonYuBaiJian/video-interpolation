a991553930eacbcc2693fb03a7f661f9a1ec1a6c
Samson Yu Bai Jian <36767958+SamsonYuBaiJian@users.noreply.github.com>
2020-08-08 21:50:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -81,12 +81,9 @@ if __name__ == '__main__':
         
         autoencoder.train()
         discriminator.train()
+        start_time = time.time()
         for i in trainloader:
             # load data
-            if args.time_it:
-                # start_time = time.time()
-                print(datetime.now().time())
-
             first = i['first_last_frames_flow'][0]
             last = i['first_last_frames_flow'][1]
             flow = i['first_last_frames_flow'][2]
@@ -120,11 +117,12 @@ if __name__ == '__main__':
                     break
 
             if args.time_it:
-                print(datetime.now().time())
-                # time_taken = time.time() - start_time
-                # train_batches = int(np.ceil(float(args.max_num_images) / args.batch_size))
-                # print('Epoch [{} / {}] Time per batch of {}: {} seconds --> {} seconds per epoch for {} batches'.format(epoch+1, args.num_epochs, mid.shape[0], 
-                #     time_taken, time_taken * train_batches, train_batches))
+                time = time.time()
+                time_taken = time - start_time
+                start_time = time
+                train_batches = int(np.ceil(float(args.max_num_images) / args.batch_size))
+                print('Epoch [{} / {}] Time per batch of {}: {} seconds --> {} seconds per epoch for {} batches'.format(epoch+1, args.num_epochs, mid.shape[0], 
+                    time_taken, time_taken * train_batches, train_batches))
 
         train_loss_epoch[0] /= num_batches
         train_loss_epoch[1] /= num_batches
