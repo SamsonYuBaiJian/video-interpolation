1751783a1856aa826e20caaac990cce86814500a
Samson Yu Bai Jian <36767958+SamsonYuBaiJian@users.noreply.github.com>
2020-08-13 17:59:51 +0800

    Add experiment results

--- plot_stats.py
+++ plot_stats.py
@@ -17,24 +17,24 @@ def plot_stats(exp_dir, save):
     # Plot stats
     epoch_interval = hyperparams['eval_every']
 
-    train_g_loss = [i[0] for i in stats['train_loss']]
+    train_g_loss = [i[0] * 100 for i in stats['train_loss']]
     train_d_loss_real = [i[1] for i in stats['train_loss']]
     train_d_loss_fake = [i[2] for i in stats['train_loss']]
-    val_g_loss = [i[0] for i in stats['val_loss']]
+    val_g_loss = [i[0] * 100 for i in stats['val_loss']]
     val_d_loss_real = [i[1] for i in stats['val_loss']]
-    val_d_loss_fake = [i[2] for i in stats['train_loss']]
+    val_d_loss_fake = [i[2] for i in stats['val_loss']]
     length = len(stats['train_loss'])
     epochs = [epoch_interval * i + 1 for i in range(length)]
 
     _, axes = plt.subplots(1, 2)
-    axes[0].set_title('Custom RRIN Loss vs Epoch')
-    axes[1].set_title('Discriminator Loss vs Epoch')
-    axes[0].plot(epochs, train_g_loss, label='Train loss')
-    axes[0].plot(epochs, val_g_loss, label='Val loss')
-    axes[1].plot(epochs, train_d_loss_real, label='Train loss - real')
-    axes[1].plot(epochs, train_d_loss_fake, label='Train loss - fake')
-    axes[1].plot(epochs, val_d_loss_real, label='Val loss - real')
-    axes[1].plot(epochs, val_d_loss_fake, label='Val loss - fake')
+    axes[0].set_title('Train Loss vs Epoch')
+    axes[1].set_title('Val Loss vs Epoch')
+    axes[0].plot(epochs, train_g_loss, label='Custom RRIN (x100)')
+    axes[0].plot(epochs, train_d_loss_real, label='D - real')
+    axes[0].plot(epochs, train_d_loss_fake, label='D - fake')
+    axes[1].plot(epochs, val_g_loss, label='Custom RRIN (x100)')
+    axes[1].plot(epochs, val_d_loss_real, label='D - real')
+    axes[1].plot(epochs, val_d_loss_fake, label='D - fake')
     axes[0].set_xticks(epochs)
     axes[1].set_xticks(epochs)
     axes[0].legend()
